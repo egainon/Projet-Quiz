f3e839746634fef8dc6a3a9232bad0bff2f6f411
Chau <hoaichau20@hotmail.com>
2025-10-28 15:10:15 +0100

    constante option button

--- game.js
+++ game.js
@@ -1,11 +1,24 @@
 import { quiz_Ghibli } from "./question.js";
 
-const document
-
 const quizContainer = document.getElementById("quiz-container");
 const quizQuestion = document.getElementsByClassName("question");
 const quizOption = document.getElementsByClassName("options");
 const quizButton = document.getElementsByClassName("button-container");
 const nextButton = document.getElementById("next-button");
 
-console.log(quizContainer.textContent)
+console.log(quizContainer, quizQuestion, quizOption, quizButton, nextButton);
+
+const firstQuestion = quiz_Ghibli.questions[0];
+
+firstQuestion.innerText = firstQuestion;
+
+console.log(firstQuestion);
+
+firstQuestion.options.forEach((option) => {
+  const option_btn = document.createElement("button");
+  option_btn.innerText = option;
+  option_btn.classList.add("options");
+  quizOption.appendChild("option_btn");
+});
+
+console.log(quizOption);
