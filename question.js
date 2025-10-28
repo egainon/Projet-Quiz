dee58279a3faf2f0367510e1a0fae7c4d7b03824
egainon <miliegainon@hotmail.fr>
2025-10-28 12:24:09 +0100

    premier save

--- question.js
+++ question.js
@@ -8,18 +8,47 @@ export const quiz_Ghibli = {
         "Un renard",
         "Un loup"
       ],
-      correct_answer: "L'astronomie, spécialisée dans les débris spatiaux."
+      correct_answer: "Un loup"
     },
     {
-      text: "En plus de son travail d'astronome, dans quelle autre activité Fatoumata Kébé est-elle impliquée ?",
+      text: "Quel film de Ghibli met en scène une jeune fille indépendante qui s’installe seule dans une nouvelle ville pour exercer son métier ?",
       options: [
-        "Elle enseigne l'astrophotographie aux astronautes de la Station spatiale internationale.",
-        "Elle milite pour l'éducation des jeunes filles en astronomie.",
-        "Elle entraîne des robots pour des compétitions de quiz interstellaires.",
-        "Elle développe des applications pour cultiver des potagers en apesanteur."
+        "Nausicaä de la Vallée du Vent",
+        "Kiki la petite sorcière",
+        "Le Château ambulant",
+        "Arietty, le petit monde des chapardeurs"
       ],
-      correct_answer: "Elle milite pour l'éducation des jeunes filles en astronomie."
+      correct_answer: "Kiki la petite sorcière"
     },
-    // Autres questions...
+    {
+      text: "Qui est le compositeur emblématique des musiques des films de Hayao Miyazaki ?",
+      options: [
+        "Ryuichi Sakamoto",
+        "Yoko Kanno",
+        "Joe Hisaishi",
+        "Nobuo Uematsu"
+      ],
+      correct_answer: "Joe Hisaishi"
+    },
+    {
+      text: "Dans Le Voyage de Chihiro, quel plat les parents de Chihiro mangent avant de se transformer en cochons ?",
+      options: [
+        "Des nouilles au miso",
+        "Des boulettes de riz",
+        "Des plats de viande destinés aux esprits",
+        "Des tempuras de légumes"
+      ],
+      correct_answer: "Des plats de viande destinés aux esprits"
+    },
+    {
+      text: "Quel objet apparaît discrètement dans plusieurs films du Studio Ghibli, comme un clin d’œil entre réalisateurs ?",
+      options: [
+        "Une montre cassée",
+        "Le bus-chat (Neko Bus)",
+        "Une boîte à musique",
+        "Un parapluie rouge"
+      ],
+      correct_answer: "Le bus-chat (Neko Bus)"
+    },
   ]
 };
