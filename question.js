6fa63d0c437b01f459521dbb1967b43894d5ef83
egainon <miliegainon@hotmail.fr>
2025-10-28 11:34:47 +0100

    cretation fichier game

---
+++ question.js
@@ -0,0 +1,25 @@
+export const quiz_Ghibli = {
+  questions: [
+    {
+      text: "Quel animal accompagne la princesse Mononoké tout au long du film ?",
+      options: [
+        "Un corbeau",
+        "Un chat",
+        "Un renard",
+        "Un loup"
+      ],
+      correct_answer: "L'astronomie, spécialisée dans les débris spatiaux."
+    },
+    {
+      text: "En plus de son travail d'astronome, dans quelle autre activité Fatoumata Kébé est-elle impliquée ?",
+      options: [
+        "Elle enseigne l'astrophotographie aux astronautes de la Station spatiale internationale.",
+        "Elle milite pour l'éducation des jeunes filles en astronomie.",
+        "Elle entraîne des robots pour des compétitions de quiz interstellaires.",
+        "Elle développe des applications pour cultiver des potagers en apesanteur."
+      ],
+      correct_answer: "Elle milite pour l'éducation des jeunes filles en astronomie."
+    },
+    // Autres questions...
+  ]
+};
