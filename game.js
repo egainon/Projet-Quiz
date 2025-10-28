908c283248c6ced5bfe131e7a66d626cd6ea2dde
Chau <hoaichau20@hotmail.com>
2025-10-28 15:43:18 +0100

    15:45

--- game.js
+++ game.js
@@ -1,16 +1,15 @@
 import { quiz_Ghibli } from "./question.js";
 
 const quizContainer = document.getElementById("quiz-container");
-const quizQuestion = document.getElementsByClassName("question");
-const quizOption = document.getElementsByClassName("options");
-const quizButton = document.getElementsByClassName("button-container");
+const quizQuestion = document.querySelector(".question");
+const quizOption = document.querySelector(".options");
 const nextButton = document.getElementById("next-button");
 
-console.log(quizContainer, quizQuestion, quizOption, quizButton, nextButton);
+console.log(quizContainer, quizQuestion, quizOption, nextButton);
 
 const firstQuestion = quiz_Ghibli.questions[0];
 
-firstQuestion.innerText = firstQuestion;
+quizQuestion.innerText = firstQuestion;
 
 console.log(firstQuestion);
 
@@ -18,7 +17,7 @@ firstQuestion.options.forEach((option) => {
   const option_btn = document.createElement("button");
   option_btn.innerText = option;
   option_btn.classList.add("options");
-  quizOption.appendChild("option_btn");
+  quizOption.appendChild(option_btn);
 });
 
 console.log(quizOption);
