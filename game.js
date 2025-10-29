ced74566d13941e564e720cb179fc29d68e2e7f5
Chau <hoaichau20@hotmail.com>
2025-10-29 10:51:18 +0100

    etape5

--- game.js
+++ game.js
@@ -1,23 +1,46 @@
 import { quiz_Ghibli } from "./question.js";
 
+let currentQuestionIndex = 0; 
+
 const quizContainer = document.getElementById("quiz-container");
 const quizQuestion = document.querySelector(".question");
 const quizOption = document.querySelector(".options");
 const nextButton = document.getElementById("next-button");
 
-console.log(quizContainer, quizQuestion, quizOption, nextButton);
+function loadQuestion () {
+    quizQuestion.innerHTML = '';
+    const currentQuestion = quiz_Ghibli.questions[currentQuestionIndex];
+    quizQuestion.innerText = currentQuestion.text;
 
-const firstQuestion = quiz_Ghibli.questions[0];
+    currentQuestion.options.forEach(option => {
+    const option_btn = document.createElement('button');
+    option_btn.innerText = option;
+    option_btn.classList.add("options");
+    quizOption.appendChild(option_btn);
+    });
 
-quizQuestion.innerText = firstQuestion.text;
+    nextButton.addEventListener('click', () => {
+    currentQuestionIndex++;
+    if(currentQuestionIndex < currentQuestionIndex.questions.length) {
+    loadQuestion();
+    } else {
+      quizQuestion.innerText = 'fin du quiz';
+      quizOption.innerHTML = '';
+      nextButton.style.display = 'none';
+    }
+    }
+);
 
-console.log(firstQuestion);
 
-firstQuestion.options.forEach((option) => {
-  const option_btn = document.createElement("button");
-  option_btn.innerText = option;
-  option_btn.classList.add("options");
-  quizOption.appendChild(option_btn);
-});
+} 
 
-console.log(quizOption);
+
+loadQuestion();
+
+
+
+
+
+
+
+
